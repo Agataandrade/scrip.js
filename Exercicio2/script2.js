432c732105619655e3881d25169fa219316cae5d
Agata Andrade <agatasouandrade@gmail.com>
2025-07-09 16:46:53 -0300

    Update script2.js

--- Exercicio2/script2.js
+++ Exercicio2/script2.js
@@ -1,8 +1,10 @@
-//Nota do Aluno
-let nota1 = 10.0;
-let nota2 = 8.0;
-let nota3 = 7.0;
-let nota4 = 7.5;
+import ler = require("readline-sync");
+
+// Nota do aluno
+let nota1 = ler.questionFloat("Digite a nota 1: ");
+let nota2 = ler.questionFloat("Digite a nota 2: ");
+let nota3 = ler.questionFloat("Digite a nota 3: ");
+let nota4 = ler.questionFloat("Digite a nota 4: ");
 // Soma de todas as notas
 let mediaFinal = ((nota1 + nota2 + nota3 + nota4) /4);
 //Resultado final
